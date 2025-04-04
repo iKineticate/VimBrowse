57d85826b97699e54944c1b6d2ac885cbf2d8e8f
iKineticate <115683118+iKineticate@users.noreply.github.com>
2025-04-04 15:51:09 +0800

    Update hotkey.rs

--- src/hotkey.rs
+++ src/hotkey.rs
@@ -1,6 +1,6 @@
 use windows::Win32::UI::Input::KeyboardAndMouse::{
-    INPUT, INPUT_0, INPUT_KEYBOARD, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP, SendInput,
-    VIRTUAL_KEY,
+    INPUT, INPUT_0, INPUT_KEYBOARD, INPUT_MOUSE, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP,
+    MOUSEEVENTF_WHEEL, MOUSEINPUT, SendInput, VIRTUAL_KEY,
 };
 
 pub fn send_keys(inputs: &[VIRTUAL_KEY]) {
@@ -28,5 +28,25 @@ pub fn send_keys(inputs: &[VIRTUAL_KEY]) {
 
     std::thread::spawn(move || unsafe {
         SendInput(&inputs, std::mem::size_of::<INPUT>() as i32);
+    });
+}
+
+pub fn send_mouse_scoll(delta: i32) {
+    let input = INPUT {
+        r#type: INPUT_MOUSE,
+        Anonymous: INPUT_0 {
+            mi: MOUSEINPUT {
+                dx: 0,
+                dy: 0,
+                mouseData: (delta * 120) as u32, // WHEEL_DELTA=120
+                dwFlags: MOUSEEVENTF_WHEEL,
+                time: 0,
+                dwExtraInfo: 0,
+            },
+        },
+    };
+
+    std::thread::spawn(move || unsafe {
+        SendInput(&[input], std::mem::size_of::<INPUT>() as i32);
     });
 }
